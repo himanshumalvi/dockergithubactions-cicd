aa5bf86ab28ee11144ced304a26c5750e8990576
Malvi.Himanshu <himanshu.malvi@philips.com>
2022-01-04 18:00:44 +0530

    Hello Changes deepak

--- src/main/java/com/dockerdec31/dockerspringboot/cicd/DockerMain.java
+++ src/main/java/com/dockerdec31/dockerspringboot/cicd/DockerMain.java
@@ -10,7 +10,7 @@ public class DockerMain {
 
     @GetMapping("/helloDocker")
     public String getStart(){
-        return "Hello Aparna"+ new Date().toGMTString();
+        return "Hello Deepak"+ new Date().toGMTString();
     }
 
 }
